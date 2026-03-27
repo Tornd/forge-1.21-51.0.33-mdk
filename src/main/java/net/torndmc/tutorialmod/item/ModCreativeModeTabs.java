cd0e6c55ee3408644e31c8550a5f2d0685a4e7e6
torndmc <torndyt@gmail.com>
2026-03-27 14:57:59 +0100

    Creative Tabs

--- src/main/java/net/torndmc/tutorialmod/item/ModCreativeModeTabs.java
+++ src/main/java/net/torndmc/tutorialmod/item/ModCreativeModeTabs.java
@@ -15,16 +15,39 @@ public class ModCreativeModeTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
             DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TutorialMod.MOD_ID);
 
-    public static final RegistryObject<CreativeModeTab> TUTORIAL_TAB = CREATIVE_MODE_TABS.register("tutorial_tab", () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.ALEXANDRITE.get()))
-            .title(Component.translatable("creativetab.tutorial_tab"))
-            .displayItems((pParameters, pOutput) -> {
-                pOutput.accept(ModItems.ALEXANDRITE.get());
-                pOutput.accept(ModItems.RAW_ALEXANDRITE.get());
-                pOutput.accept(ModBlocks.ALEXANDRITE_BLOCK.get());
-                pOutput.accept(ModBlocks.RAW_ALEXANDRITE_BLOCK.get());
-                pOutput.accept(Items.DIAMOND);
-            })
-            .build());
+    public static final RegistryObject<CreativeModeTab> MOD_TAB = CREATIVE_MODE_TABS.register("mod_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModBlocks.RAW_ALEXANDRITE_BLOCK.get()))
+
+                    .title(Component.translatable("creativetab.tutorialmod.mod"))
+                    .displayItems((pParameters, pOutput) -> {
+                        pOutput.accept(ModItems.ALEXANDRITE.get());
+                        pOutput.accept(ModItems.RAW_ALEXANDRITE.get());
+                        pOutput.accept(ModBlocks.ALEXANDRITE_BLOCK.get());
+                        pOutput.accept(ModBlocks.RAW_ALEXANDRITE_BLOCK.get());
+
+                    }).build());
+
+    public static final RegistryObject<CreativeModeTab> ALEXANDRITE_ITEMS_TAB = CREATIVE_MODE_TABS.register("alexandrite_items_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.ALEXANDRITE.get()))
+                    .withTabsBefore(MOD_TAB.getId())
+                    .title(Component.translatable("creativetab.tutorialmod.alexandrite_items"))
+                    .displayItems((pParameters, pOutput) -> {
+                    pOutput.accept(ModItems.ALEXANDRITE.get());
+                    pOutput.accept(ModItems.RAW_ALEXANDRITE.get());
+
+                }).build());
+
+    public static final RegistryObject<CreativeModeTab> ALEXANDRITE_BLOCKS_TAB = CREATIVE_MODE_TABS.register("alexandrite_blocks_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModBlocks.ALEXANDRITE_BLOCK.get()))
+                    .withTabsBefore(ALEXANDRITE_ITEMS_TAB.getId())
+                    .title(Component.translatable("creativetab.tutorialmod.alexandrite_blocks"))
+                    .displayItems((pParameters, pOutput) -> {
+                        pOutput.accept(ModBlocks.ALEXANDRITE_BLOCK.get());
+                        pOutput.accept(ModBlocks.RAW_ALEXANDRITE_BLOCK.get());
+
+                    }).build());
+
+
 
     public static void register(IEventBus eventbus) {
         CREATIVE_MODE_TABS.register(eventbus);
